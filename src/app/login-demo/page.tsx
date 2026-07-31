edea392dc3f67cd1a30ed9cc003d4592562ed96e
Naufal-elku <naufalugil@gmail.com>
2026-07-31 17:06:01 +0700

    vale

--- src/app/login-demo/page.tsx
+++ src/app/login-demo/page.tsx
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-export default function LoginDemoPage () {
-    return (
-        <h2 className="text-9xl">Demo Demo Flash Peak</h2>
-    )
-}
-
-// buat halaman demo
-=======
 import React from 'react'
 
-function page() {
+export default function page() {
   return (
     <div>page</div>
   )
 }
-
-export default page
->>>>>>> a94f3fed634152483de3ff12719c5445f72ddcab
