36bb65ef10abbaa4843a07a9e430e47107758e53
Naufal-elku <naufalugil@gmail.com>
2026-08-06 14:20:41 +0700

    commit10

--- src/components/common/searchBlog.tsx
+++ src/components/common/searchBlog.tsx
@@ -3,7 +3,7 @@ import search from "@/picture/search.png"
 
 export default function SearchBlog() {
     return (
-        <div className="flex items-center w-full max-w-[439px] h-[44px] gap-2 mx-auto h-auto mt-8">
+        <div className="flex items-center w-full max-w-[439px] gap-2 mx-auto h-auto mt-3">
             <div className="relative flex items-center flex-1 h-full">
                 <img
                     src={search.src}
@@ -16,7 +16,7 @@ export default function SearchBlog() {
                     className="w-full h-full font-normal text-[16px] placeholder-[#B6B6B6] bg-background rounded-lg pl-11 py-2.5 gap-2 border border-border-btn focus:outline-none focus:border-[#007AFF] transition-colors"
                 />
             </div>
-            <button className="h-[44px] w-[88px] px-[18px] py-[10px] rounded-[8px] flex items-center justify-center gap-2 bg-blue-600 text-white text-base font-medium hover:bg-blue-700 transition-colors">
+            <button className="h-11 w-22 px-4.5 py-2.5 rounded-[8px] flex items-center justify-center gap-2 bg-blue-600 text-white text-base font-medium hover:bg-blue-700 transition-colors">
                 Cari
             </button>
         </div>
